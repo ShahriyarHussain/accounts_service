c9b08c53639a7e936bfb47c60405cf357ae7dc6f
ShahriyarHussain <shossainshihab36@gmail.com>
2022-04-12 12:59:43 +0600

    Update security config

--- src/account/Config/AccountsSecurityConfig.java
+++ src/account/Config/AccountsSecurityConfig.java
@@ -1,8 +1,6 @@
 package account.Config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,10 +11,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.access.AccessDeniedHandler;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Map;
 
 @EnableWebSecurity
 public class AccountsSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -64,16 +58,6 @@ public class AccountsSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     AuthenticationEntryPoint getAuthenticationEntryPoint() {
-        return (request, response, authException) ->
-        {
-            response.setStatus(HttpStatus.UNAUTHORIZED.value());
-            response.getOutputStream().println(new ObjectMapper().writeValueAsString(Map.of(
-                    "timestamp", DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(LocalDateTime.now()),
-                    "status", 401,
-                    "error", "Unauthorized",
-                    "message", "User account is locked",
-                    "path", request.getRequestURI()
-            )));
-        };
+        return new CustomAuthenticationEntryPointHandler();
     }
 }
---
+++ src/account/Config/CustomAuthenticationEntryPointHandler.java
@@ -0,0 +1,30 @@
+package account.Config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+
+public class CustomAuthenticationEntryPointHandler implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.getOutputStream().println(new ObjectMapper().writeValueAsString(Map.of(
+                "timestamp", DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(LocalDateTime.now()),
+                "status", 401,
+                "error", "Unauthorized",
+                "message", "User account is locked",
+                "path", request.getRequestURI()
+        )));
+    }
+}
